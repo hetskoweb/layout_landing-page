4de83adfb7308fcdb698480c15d32bf7070b50e4
Illia <hetsko.ilya@gmail.com>
2025-01-24 23:05:05 +0200

    make build

---
+++ index.01433a18.js
@@ -0,0 +1,2 @@
+function o(){"#menu"===window.location.hash?document.body.classList.add("scroll-lock"):document.body.classList.remove("scroll-lock")}window.addEventListener("hashchange",o),o();
+//# sourceMappingURL=index.01433a18.js.map
